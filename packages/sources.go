50cc206e506ec2406dd700c6aef438802015ca5d
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-02-06 20:20:59 +0530

    First cut version of sources + tests

---
+++ packages/sources.go
@@ -0,0 +1,44 @@
+package packages
+
+import (
+  // "io/ioutil"
+  // "fmt"
+  "encoding/json"
+)
+
+type Repository struct {
+  // Name of the source - eg. universe
+  Name  string    `json:"name"`
+  // Location of the source - eg. github.com/ashwanthkumar/marathonctl-universe
+  // We support all the protocols supported by https://github.com/hashicorp/go-getter
+  Loc   string    `json:"loc"`
+}
+
+// Repositories are the source of truth of all the package repositories in the system. 
+// General location of the file is ~/.marathonctl/repos.source
+type Repositories []Repository
+
+// Do we already know this repo?
+func (r *Repositories) Exists(repo string) bool {
+  for _, repository := range *r {
+    if repository.Name == repo {
+      return true
+    }
+  }
+  return false
+}
+
+func (r *Repositories) Add(repo Repository) *Repositories {
+  *r = append(*r, repo)
+  return r
+}
+
+func (r *Repositories) Serialize() ([]byte, error) {
+  return json.Marshal(r)
+}
+
+func Deserialize(data []byte) (*Repositories, error) {
+  var repos Repositories
+  err := json.Unmarshal(data, &repos)
+  return &repos, err
+}
---
+++ packages/sources_test.go
@@ -0,0 +1,57 @@
+package packages
+
+import (
+  "testing"
+  "github.com/stretchr/testify/assert"
+)
+
+func TestRepositoriesExist(t *testing.T) {
+  repos := newRepositories([]Repository {
+    repo("r1", "loc1"), repo("r2", "loc2"),
+  })
+  assert.Equal(t, repos.Exists("r1"), true)
+  assert.Equal(t, repos.Exists("r3"), false)
+}
+
+func TestRepositoriesAdd(t *testing.T) {
+  repos := newRepositories([]Repository {
+    repo("r1", "loc1"), repo("r2", "loc2"),
+  })
+  assert.Equal(t, repos.Exists("r3"), false)
+  repos.
+    Add(repo("r3", "loc3")).
+    Add(repo("r4", "loc4"))
+  assert.Equal(t, repos.Exists("r3"), true)
+  assert.Equal(t, repos.Exists("r4"), true)
+}
+
+func TestRepositoriesSerialize(t *testing.T) {
+  repos := newRepositories([]Repository {
+    repo("universe", "github.com/ashwanthkumar/marathonctl-universe"),
+  })
+  expectedJson := `[{"name":"universe","loc":"github.com/ashwanthkumar/marathonctl-universe"}]`
+  result, err := repos.Serialize()
+  assert.NoError(t, err)
+  assert.Equal(t, []byte(expectedJson), result)
+}
+
+func TestDeserialize(t *testing.T) {
+  reposInJson := `[{
+    "name": "universe",
+    "loc": "github.com/ashwanthkumar/marathonctl-universe"
+  }]`
+  repos, err := Deserialize([]byte(reposInJson))
+  assert.NoError(t, err)
+  assert.Equal(t, repos.Exists("universe"), true)
+}
+
+func newRepositories(repos []Repository) Repositories {
+  return repos
+}
+
+func repo(name, loc string) Repository {
+  return Repository {
+    Name: name,
+    Loc: loc,
+  }
+}
