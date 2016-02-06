7bd1f366d44cee3de8a6a7ee3e092b1e91542f49
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2016-02-06 21:51:55 +0530

    package update now can update specific repository

--- cmd/package_update.go
+++ cmd/package_update.go
@@ -6,13 +6,18 @@ import (
 )
 
 var PackageUpdate = &cobra.Command{
-  Use:   "update",
+  Use:   "update [repository]",
   Short: "Update the local package cache",
   Long:  "Update the local package cache",
   Run: AttachHandler(updatePackageCache),
 }
 
 func updatePackageCache(args []string) (err error) {
+  if len(args) > 0 {
+    repository := args[0]
+    return packages.Update(repository)
+  }
+
   return packages.UpdateAll()
 }
 
